f129efcba12e0461618327b73291e5eb51aeed1d
GuevarraJosh <167516344+GuevarraJosh@users.noreply.github.com>
2024-04-19 17:30:06 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,46 @@ const store = createStore({
   state: {
     products: []
   },
+  mutations: {
+    ADD_PRODUCT(state, newProduct) {
+      state.products.push(newProduct);
+    },
+    EDIT_PRODUCT(state, updatedProduct) {
+      const index = state.products.findIndex(p => p.id === updatedProduct.id);
+      if (index !== -1) {
+        state.products.splice(index, 1, updatedProduct);
+      }
+    },
+    DELETE_PRODUCT(state, productId) {
+      state.products = state.products.filter(product => product.id !== productId);
+    }
+  },
+  actions: {
+    addProduct({ commit }, newProduct) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('ADD_PRODUCT', newProduct);
+          resolve();
+        }, 1000);
+      });
+    },
+    editProduct({ commit, state }, updatedProduct) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('EDIT_PRODUCT', updatedProduct);
+          resolve();
+        }, 1000);
+      });
+    },
+    deleteProduct({ commit }, productId) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('DELETE_PRODUCT', productId);
+          resolve();
+        }, 1000);
+      });
+    }
+  },
   getters: {
     getProductById: (state) => (id) => {
       return state.products.find(product => product.id === id);
