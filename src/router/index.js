2d91914f9a15958f43f46df6b0b897e0ff087ae0
haellyz28 <164413182+haellyz28@users.noreply.github.com>
2024-04-19 17:11:43 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-]
+  {
+    path: '/',
+    redirect: '/products'
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: () => import(/* webpackChunkName: "ProductListView" */ '../views/ProductListView.vue')
+  },
+  {
+    path: '/add-product',
+    name: 'AddProduct',
+    component: () => import(/* webpackChunkName: "AddProductView" */ '../views/AddProductView.vue')
+  },
+  {
+    path: '/edit-product/:id',
+    name: 'EditProduct',
+    component: () => import(/* webpackChunkName: "EditProductView" */ '../views/EditProductView.vue'),
+    props: true
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
